460c6a202faff5c396501f5e47145e7f9316db1c
Laura5513 <lauramanuela.rodriguez@gmail.com>
2023-03-01 18:10:21 -0500

    Script

---
+++ scripts/Data.R
@@ -0,0 +1,19 @@
+#**************************************************************************************#
+#                                    TALLER 3 BDML                                     #
+#                        Uniandes: Sofia Charry Tobar                                  #
+#                                  Laura Manuela Rodríguez Morales                     #
+#                                  Nicol Valeria Rodríguez Rodríguez                   #
+#                                  Brayan Alexander Vargas Rojas                       #
+#                          Fuente: Properati,                                            #
+#**************************************************************************************#
+
+# Limpiar el espacio
+rm(list = ls(all.names = TRUE))
+
+#setwd("C:/Users/nicol/Documents/GitHub/Repositorios/Taller-3-BDML")
+#setwd("/Users/bray/Desktop/Big Data/Talleres/Taller-3-BDML")
+#setwd("C:/Users/lmrod/OneDrive/Documentos/GitHub/Taller-3-BDML")
+
+
+require(pacman)
+p_load()
