fc3774636b5fe04eb45ff1d222cccf5fbd2ac6bb
Laura5513 <lauramanuela.rodriguez@gmail.com>
2023-03-01 19:05:30 -0500

    + script

--- scripts/Data.R
+++ scripts/Data.R
@@ -10,44 +10,50 @@
 # Limpiar el espacio
 rm(list = ls(all.names = TRUE))
 
+# ------------------------------------------------------------------------------------ #
+# Cargar librerias.
+# ------------------------------------------------------------------------------------ #
+
 #setwd("C:/Users/nicol/Documents/GitHub/Repositorios/Taller-3-BDML")
 #setwd("/Users/bray/Desktop/Big Data/Talleres/Taller-3-BDML")
 #setwd("C:/Users/lmrod/OneDrive/Documentos/GitHub/Taller-3-BDML")
 
 
-
-list.of.packages = c("pacman", "readr","tidyverse", "dplyr", "arsenal", "fastDummies", 
-                     "caret", "glmnet", "MLmetrics", "skimr", "plyr", "stargazer", 
-                     "ggplot2", "corrplot", "Hmisc")
-
-new.packages = list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
-sapply(list.of.packages, require, character.only = TRUE)
-
-# Precio 
-precio_bog <- read.csv("./data/submission_template.csv")
-# Train
-train_bog <- read_csv("./data/train.csv")
-# Test 
-test_bog <- read_csv("./data/test.csv")
-
+require(pacman)
+p_load(tidyverse, sf, tmaptools, osmdata)
 
 chapinero<-opq(bbox = getbb("Chapinero Bogotá Colombia"))
 chapinero
 
+# ------------------------------------------------------------------------------------ #
+# Compilar la base de datos
+# ------------------------------------------------------------------------------------ #
+
+available_features() %>% head(20)
+available_tags() %>% head(20)
+
+available_tags("amenity")
+
+#Obtenenemos las universidades
+universidades <- chapinero %>% 
+  add_osm_feature(key="amenity",value="university") %>% # de las amenities disponibles, seleccionamos las universidades
+  osmdata_sf() #transformamos a un objeto sf
+
+puntos_universidades<-universidades$osm_point
+head(puntos_universidades)
+
+ggplot()+
+  geom_sf(data=puntos_universidades) +
+  theme_bw()
+
+
+p_load("leaflet")
+
+leaflet() %>% 
+  addTiles() %>%  #capa base
+  addCircles(data=puntos_universidades, popup = ~name)
+
 #chapinero_bbox <- c(-74.066598, 4.631753, -74.061713, 4.658588)
-"chapinero_osm <- osmdata::osmdata_download(bbox = chapinero_bbox, timeout = 60)
-
-
-
-
-
-
-
-
-
-
-
-
+#chapinero_osm <- osmdata::osmdata_download(bbox = chapinero_bbox, timeout = 60)
 
 
